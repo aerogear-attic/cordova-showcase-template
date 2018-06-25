6c39d9f2dbf288012c22f2022b53d0c2d11e2903
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-06-25 08:38:26 +0100

    Add SSO docs link

--- src/pages/sso/sso.ts
+++ src/pages/sso/sso.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { ToastController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 
 @Component({
@@ -10,10 +10,13 @@ import { ToastController } from 'ionic-angular';
 export class SSOPage {
     pageTitle: string;
 
-    constructor(public toastCtrl: ToastController, public navCtrl: NavController, public navParams: NavParams) {
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
         this.navCtrl = navCtrl;
         this.pageTitle = navParams.get('linkParam');
     }
 
-    ionViewDidEnter(): void {}
+    ionViewWillEnter(): void { 
+        window.open('https://docs.aerogear.org/aerogear/latest/keycloak/index.html?sso=1', '_system')
+        this.navCtrl.setRoot(HomePage);
+      }
 }
