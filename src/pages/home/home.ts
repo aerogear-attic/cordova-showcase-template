de27473b2877d3c169bc330e81a33827bd9ab165
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-07-09 11:52:40 +0100

    lint fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,14 @@
 import { Auth } from "@aerogear/auth";
+import { ServiceConfiguration } from "@aerogear/core";
 import { Component } from "@angular/core";
+import { Dialogs } from "@ionic-native/dialogs";
+import { HTTP } from "@ionic-native/http";
 import { NavController, Platform } from "ionic-angular";
 import { authProvider } from "../../services/auth.service";
 import { DocumentationService } from "../../services/documentation.service";
 import { PushService } from "../../services/push.service";
 import { PushMessage } from "../pushMessages/message";
 import { PushMessagesPage } from "../pushMessages/pushMessages";
-import { ServiceConfiguration } from "@aerogear/core";
-import { Dialogs } from "@ionic-native/dialogs";
-import { HTTP } from "@ionic-native/http";
-
 
 declare var url: any;
 declare var require: any;
@@ -24,7 +23,6 @@ const appConfig = require("../../mobile-services.json");
 export class HomePage {
 
   private configuration: ServiceConfiguration = appConfig.services[0];
-  private readonly SSL_ERROR: string = "SSL handshake failed";
 
   constructor(
     private navCtrl: NavController,
@@ -53,25 +51,25 @@ export class HomePage {
     this.navCtrl.push(PushMessagesPage);
   }
 
-  public checkSSL(){
-    if ( appConfig.services.length > 0 ) {
+  public checkSSL() {
+    if (appConfig.services.length > 0) {
       this.http.get(this.configuration.url, {}, {})
-      .then(() => {
-        console.info("SSL handshake successfull");
-      }).catch((error) => {
-        if (error.status === -1 ) {
-          this.dialog.confirm(
-            `You may be using self signed certificates that will prevent the showcase from running properly.` +
-            ` Please review the documentation and configure your certificates.`,
-            "Certificate Error",
-            ["Show Documentation", "Close"],
-          ).then((result) => {
-            if (result === 1) {
-              this.docService.open(DocumentationService.SELF_SIGNED_DOCS);
-            }
-          });
-        }
-      });
+        .then(() => {
+          console.info("SSL handshake successfull");
+        }).catch((error) => {
+          if (error.status === -1) {
+            this.dialog.confirm(
+              `You may be using self signed certificates that will prevent the showcase from running properly.` +
+              ` Please review the documentation and configure your certificates.`,
+              "Certificate Error",
+              ["Show Documentation", "Close"],
+            ).then((result) => {
+              if (result === 1) {
+                this.docService.open(DocumentationService.SELF_SIGNED_DOCS);
+              }
+            });
+          }
+        });
     }
   }
 }
--- src/services/documentation.service.ts
+++ src/services/documentation.service.ts
@@ -11,7 +11,7 @@ export class DocumentationService {
   public static readonly URL_PUSH: string = "https://docs.aerogear.org/external/showcase/cordova/push.html";
   public static readonly URL_DEVICE_SECURITY: string = "https://docs.aerogear.org/external/showcase/cordova/security.html";
   public static readonly SELF_SIGNED_DOCS: string = "https://docs.aerogear.org/external/cert/self-signed-cert.html";
-  
+
   constructor(private app: App) {
   }
 
